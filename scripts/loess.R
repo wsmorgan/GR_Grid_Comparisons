25b1d5ce1fb2257a1532e7b6f950268ead508a8e
Wiley Morgan <wiley.s.morgan@gmail.com>
2018-04-13 12:25:50 -0600

    Changed names to be MP, GR, and SC instead of Aflow, Mueller, and Froyen.

--- scripts/loess.R
+++ scripts/loess.R
@@ -1,6 +1,6 @@
-aflow <- read.delim("../data/R_workspace/Aflow_metals_kpd.csv",header=F,sep="\t")
-froy <- read.delim("../data/R_workspace/Froyen_metals_kpd.csv",header=F,sep="\t")
-muel <- read.delim("../data/R_workspace/Mueller_metals_kpd.csv",header=F,sep="\t")
+aflow <- read.delim("../data/R_workspace/MP_irkpd.csv",header=F,sep="\t")
+froy <- read.delim("../data/R_workspace/SC_irkpd.csv",header=F,sep="\t")
+muel <- read.delim("../data/R_workspace/GR_irkpd.csv",header=F,sep="\t")
 library("scales")
 mycol=c("green","red","blue")
 opa.a <- 0.2
@@ -18,6 +18,6 @@ x<-muel
 x<-log10(x)
 ff3<-loess.smooth(x[,1],x[,2],span=span.sm,evaluation=200)
 
-write.csv(ff1,file="../data/R_workspace/Aflow_kpd_loess.csv",row.names=FALSE)
-write.csv(ff2,file="../data/R_workspace/Froyen_kpd_loess.csv",row.names=FALSE)
-write.csv(ff3,file="../data/R_workspace/Muel_kpd_loess.csv",row.names=FALSE)
+write.csv(ff1,file="../data/R_workspace/MP_irkpd_loess.csv",row.names=FALSE)
+write.csv(ff2,file="../data/R_workspace/SC_irkpd_loess.csv",row.names=FALSE)
+write.csv(ff3,file="../data/R_workspace/GR_irkpd_loess.csv",row.names=FALSE)
